39d8c406ce3aff14237c3b48dceb535d2ae387bb
iamprakkie <gprakk.ie@gmail.com>
2023-08-25 11:42:24 -0600

    removed AMGNestedstack

--- lib/multi-new-eks-mixed-observability-construct/GrafanaOperatorConstruct.ts
+++ lib/multi-new-eks-mixed-observability-construct/GrafanaOperatorConstruct.ts
@@ -8,11 +8,6 @@ import { Construct } from 'constructs';
 import * as blueprints from '@aws-quickstart/eks-blueprints';
 import { ObservabilityBuilder } from '../common/observability-builder';
 import { GrafanaOperatorSecretAddon } from './grafanaoperatorsecretaddon';
-import { AMGNestedStack, AMGNestedStackProps } from './AMGNestedStack';
-
-//pattern wide consts
-const GITHUB_ORG = 'aws-samples';
-const WORKLOAD_REPO = `git@github.com:${GITHUB_ORG}/eks-blueprints-workloads.git`;
 
 export default class GrafanaOperatorConstruct {
 
@@ -22,27 +17,11 @@ export default class GrafanaOperatorConstruct {
         const account = inAccount! || process.env.COA_ACCOUNT_ID! || process.env.CDK_DEFAULT_ACCOUNT!;
         const region = inRegion! || process.env.COA_AWS_REGION! || process.env.CDK_DEFAULT_REGION!;
 
+        Reflect.defineMetadata("ordered", true, blueprints.addons.GrafanaOperatorAddon); //sets metadata ordered to true for GrafanaOperatorAddon        
+
         // ArgoCD configuration
         const grafanaOperatorArgoAddonConfig = createArgoAddonConfig('monitoring','https://github.com/iamprakkie/one-observability-demo.git','grafana-operator-chart');
         // const grafanaOperatorArgoAddonConfig = createArgoAddonConfig('prod', 'https://github.com/aws-samples/one-observability-demo.git','grafana-operator-manifests'); 
-        // const grafanaOperatorArgoAddonConfig = createArgoAddonConfig('dev','https://github.com/aws-samples/eks-blueprints-workloads.git','envs/dev');
-        
-        // AMG Configuration
-        const AMGNestedStackProps: AMGNestedStackProps = {
-            account: account,
-            region: region
-        }
-
-        // AMG Nested Stack AddOn
-        const amgNestedStackAddOn = new blueprints.NestedStackAddOn({
-            id: 'amg-nestedstack',
-            builder: AMGNestedStack.builder(),
-        });
-
-
-
-        Reflect.defineMetadata("ordered", true, blueprints.addons.GrafanaOperatorAddon); //sets metadata ordered to true for GrafanaOperatorAddon
-        new blueprints.ArgoCDAddOn()
 
         const addOns: Array<blueprints.ClusterAddOn> = [
             new blueprints.addons.KubeProxyAddOn(),
@@ -56,7 +35,6 @@ export default class GrafanaOperatorConstruct {
             }),
             new GrafanaOperatorSecretAddon(),
             grafanaOperatorArgoAddonConfig, // GitOps through ArgoCD
-            amgNestedStackAddOn // Creates AMG as part of Nested Stack
         ];
 
         ObservabilityBuilder.builder()
@@ -65,7 +43,9 @@ export default class GrafanaOperatorConstruct {
             .addNewClusterObservabilityBuilderAddOns()
             .addOns(...addOns)
             .build(scope, stackId);
+
     }
+
 }
 
 function createArgoAddonConfig(environment: string, repoUrl: string, repoPath?: string): blueprints.ArgoCDAddOn {
@@ -73,23 +53,9 @@ function createArgoAddonConfig(environment: string, repoUrl: string, repoPath?: 
         bootstrapRepo: {
             repoUrl: repoUrl,
             path: repoPath! || '.',
-            // path: `multi-repo/argo-app-of-apps/${environment}`, // add path if required
             targetRevision: 'main',
             // credentialsSecretName: 'github-ssh-key', // for access to private repo. This needs SecretStoreAddOn added to your cluster
             // credentialsType: 'SSH',
         },
-        // bootstrapValues: {
-        //     spec: {
-        //         service: {
-        //             type: 'LoadBalancer',
-        //         },
-        //         ingress: {
-        //             host: 'teamblueprints.com',
-        //         },                
-        //     },
-        // },
-        // values: {
-        //     server: {},
-        // },
     });
 }
